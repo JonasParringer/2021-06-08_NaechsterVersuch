db93b367e9fbc8f8b47a179f9be75558465486aa
JonasParringer <jonasparringer@gmx.de>
2021-06-11 15:45:26 +0200

    Projektdateien hinzufügen.

---
+++ Models/Document.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2021_06_08_NaechsterVersuch.Models
+{
+    public class Document
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Length { get; set; }
+        public string Author { get; set; }
+        
+        [Display(Name = "date of publication")]
+        //[DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
+        // https://docs.microsoft.com/de-de/aspnet/core/tutorials/razor-pages/new-field?view=aspnetcore-5.0&tabs=visual-studio
+        public string DataType { get; set; }
+    }
+}
