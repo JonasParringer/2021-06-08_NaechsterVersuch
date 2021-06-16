efab70f5803ebcb265849e47ea579c19d6a5d360
JonasParringer <jonasparringer@gmx.de>
2021-06-16 15:30:57 +0200

    Klappt nicht ... bin am an loggerFactory

--- Data/DocumentContext.cs
+++ Data/DocumentContext.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using _2021_06_08_NaechsterVersuch.Models;
+using Microsoft.Extensions.Logging;
 
-    public class DocumentContext : DbContext
+public class DocumentContext : DbContext
     {
+        ILoggerFactory loggerFactory;
+
+    //loggerFactory = new LoggerFactory().AddConsole()
+    // loggerFactory = new LoggerFactory().AddConsole((categoryName, logLevel) => (logLevel == LogLevel.Information) && (categoryName == DbLoggerCategory.Database.Command.Name));
+    LoggerFactory = new LoggerFactory().AddConsole((categoryName, LogLevel) => (LogLevel == LogLevel.Information) && (categoryName == DbLoggerCategory.Database.Command.Name));
+
+
+
         public DocumentContext (DbContextOptions<DocumentContext> options)
             : base(options)
         {
